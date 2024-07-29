47642711247cfc84dcaf9caa0d138e3a985a0e54
bjorgeh <bjorgen92@gmail.com>
2024-07-30 00:14:27 +0200

    Update AboutSectionTwo.tsx

--- src/components/About/AboutSectionTwo.tsx
+++ src/components/About/AboutSectionTwo.tsx
@@ -18,7 +18,7 @@ const AboutSectionTwo = () => {
               data-wow-delay=".15s"
             >
               <Image
-                src="/polarWeb/images/about/opensource.webp"
+                src="/polarWeb/main/public/images/about/barekraft.webp"
                 alt="about image"
                 width={800}
                 height={600}
@@ -26,7 +26,7 @@ const AboutSectionTwo = () => {
                 className="drop-shadow-three dark:hidden dark:drop-shadow-none"
               />
               <Image
-                src="/polarWeb/images/about/opensource.webp"
+                src="/polarWeb/main/public/images/about/barekraft.webp"
                 alt="about image"
                 width={800}
                 height={600}
