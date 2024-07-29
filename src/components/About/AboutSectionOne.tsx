b435e861ba42b560467a9ef392ae432031be4c02
bjorgeh <bjorgen92@gmail.com>
2024-07-30 00:37:29 +0200

    Update AboutSectionOne.tsx

--- src/components/About/AboutSectionOne.tsx
+++ src/components/About/AboutSectionOne.tsx
@@ -58,7 +58,7 @@ const AboutSectionOne = () => {
             <div className="w-full px-4 lg:w-1/2">
               <div className="relative mx-auto aspect-[25/24] max-w-[500px] lg:mr-0">
               <Image
-                src="/polarWeb/main/public/images/about/barekraft.webp"
+                src="/polarWeb/images/about/barekraft.webp"
                 alt="about-image"
                 width={800}
                 height={600}
@@ -66,7 +66,7 @@ const AboutSectionOne = () => {
                 className="mx-auto max-w-full drop-shadow-three dark:hidden dark:drop-shadow-none lg:mr-0"
               />
               <Image
-                src="/polarWeb/main/public/images/about/barekraft.webp"
+                src="/polarWeb/images/about/barekraft.webp"
                 alt="about-image"
                 width={800}
                 height={600}
